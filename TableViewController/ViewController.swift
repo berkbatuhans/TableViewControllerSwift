63a3b056f69b86e63753dc57c9102d7bd8e5f7bd
Berk Batuhan ŞAKAR <berkbatuhans@gmail.com>
2019-08-24 11:37:16 +0300

    Initial Commit

---
+++ TableViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewController
+//
+//  Created by Berk Batuhan ŞAKAR on 24.08.2019.
+//  Copyright © 2019 Berk Batuhan ŞAKAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
